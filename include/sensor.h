c5e55aa705a5cd693471a14a816b617e68cda8c3
Ole Sæther <osaether@gmail.com>
2025-02-28 13:24:17 +0100

    Added BME680 humidity. Some refactoring/renaming.
    Signed-off-by: Ole Sæther <osaether@gmail.com>

---
+++ include/sensor.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+void sensor_init(void);
+int32_t temperature_get(void);
+uint32_t humidity_get(void);
+uint32_t pressure_get(void);
